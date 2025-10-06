bc03c5920916bd47d5c6e77df8c1d14280d9c2e3
Luka Radisavljevic <lukaradisavljevic30@gmail.com>
2025-10-07 00:11:22 +0200

    [chore] init supa; Elaborate on docs; minor structure

---
+++ packages/database/supabase/scripts/dummy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODO:
+# This is just a placeholder script
+# Scripts could be useful in our case for:
+# - Supa version control
+# - automatic type generation for SDK
+#
+# (leaving TODO for visibility)
+
+
+echo "Dummy shell script"
---
+++ packages/shelf/README.md
@@ -0,0 +1,5 @@
+# Shelf (Docs)
+
+A (book)shelf is where all the md files of importance are found (other than the main READMEs of various packages)
+
+keep it as u wish
